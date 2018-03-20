748d5a08a6bd34676078f3c2bf0fef8c0df6b34d
thosbot <thomas@em7b5.net>
2018-03-20 09:14:08 -0400

    Add Bash functions

---
+++ bashrc.d/functions.bash
@@ -0,0 +1,21 @@
+function bg-run() {
+    cmd=$1; shift
+    $cmd "$@" >/tmp/$cmd.out 2>/tmp/$cmd.err &
+}
+
+# Syntax highlighting in the terminal
+function lesscode {
+    filename=$1
+    filetype=$2
+
+    if [ "$filetype" ]; then
+        highlight -O xterm256 -S "$filetype" "$filename" | less -R
+    else
+        highlight -O xterm256 "$filename" | less -R
+    fi
+}
+
+# Convert Markdown file to HTML and open in Lynx
+function readmarkdown {
+    pandoc --from markdown --to html5 "$1" | lynx -stdin
+}
