0b78c25379716a1eb072bb38bb0a444ada3a080e
thosbot <thomas@em7b5.net>
2018-05-20 15:43:20 -0400

    Add Openbox config

---
+++ openbox/autostart.sh
@@ -0,0 +1,11 @@
+# Set background
+xsetroot -solid "#000000" &
+
+xscreensaver &
+xclock -norender -reverse -fg white &
+workrave &
+conky &
+xterm -j -s -vb +sf +dc -cr darkgreen -u8 -geometry 109x44 &
+
+# Turn off warning bell
+xset b off &
