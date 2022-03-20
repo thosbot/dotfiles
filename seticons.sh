6e278daab4f8b46afa1948b04b1d091643b9ca8a
thosbot <thomas@em7b5.net>
2022-03-20 15:08:16 -0400

    Set icons for running apps that Openbox cannot locate

---
+++ seticons.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set icons for running apps that Openbox cannot locate for some reason
+
+xseticon -name "conky (gypsy)" /usr/share/icons/hicolor/64x64/apps/conky-sm.png
+xseticon -name "Spotify" /usr/share/icons/hicolor/64x64/apps/spotify-client.png
+xseticon -name "Workrave" /usr/share/icons/hicolor/64x64/apps/workrave.png
