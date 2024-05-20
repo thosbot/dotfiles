af64f3ea1df01cf54a91bfab94c1c6b1c4af108d
thosbot <thomas@em7b5.net>
2024-05-19 21:06:35 -0400

    Look for running process before setting icons

--- bin/set_ob_icons.sh
+++ bin/set_ob_icons.sh
@@ -2,6 +2,19 @@
 
 # Set icons for running apps that Openbox cannot locate for some reason
 
-xseticon -name "conky (gypsy)" /usr/share/icons/hicolor/64x64/apps/conky-sm.png
-xseticon -name "Spotify" /usr/share/icons/hicolor/64x64/apps/spotify-client.png
-xseticon -name "Workrave" /usr/share/icons/hicolor/64x64/apps/workrave.png
+sleep 8
+
+if pgrep -x "conky" >/dev/null
+then
+    xseticon -name "conky (gypsy)" /usr/share/icons/hicolor/64x64/apps/conky-sm.png
+fi
+
+if pgrep -x "spotify" >/dev/null
+then
+    xseticon -name "Spotify" /usr/share/icons/hicolor/64x64/apps/spotify-client.png
+fi
+
+if pgrep -x "workrave" >/dev/null
+then
+    xseticon -name "Workrave" /usr/share/icons/hicolor/64x64/apps/workrave.png
+fi
