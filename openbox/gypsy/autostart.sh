6387c87fb9d04344e43a32cdd0be06fcaca33394
thosbot <thomas@em7b5.net>
2026-01-06 10:18:08 -0500

    Mount remote drive with rclone on startup

--- openbox/gypsy/autostart.sh
+++ openbox/gypsy/autostart.sh
@@ -11,3 +11,6 @@ setxkbmap -option lv3:ralt_switch_multikey &
 
 # Turn off warning bell
 xset b off &
+
+# Mount network doc directory
+rclone --log-file=.local/var/log/rclone.log --log-level=INFO mount linode:thosdoc-5ab1c42c thosdoc &
