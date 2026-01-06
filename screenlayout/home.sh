6f1c991d680c1d6407063c9f85a48285ff46cf3a
thosbot <thomas@em7b5.net>
2026-01-06 10:17:57 -0500

    Add XRandR display settings

---
+++ screenlayout/home.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr \
+    --output eDP-1 --primary --mode 1600x900 --pos 1920x0 --rotate normal \
+    --output DP-1 --mode 1920x1200 --pos 0x0 --rotate normal \
+    --output DP-2 --off
